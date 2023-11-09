a29d5891fb001a0d040291628c2f436449574e85
Padma Patil <padma@email.com>
2023-11-09 12:23:12 -0800

    Fix js and html

--- js/scripts.js
+++ js/scripts.js
@@ -4,39 +4,59 @@ function hiddenDiv() {
     document.getElementById("jewelry3").setAttribute("class", "hidden");
 }
 
-function formValue() {
-    const program = parent(document.querySelector('input[name="jewelry"]:checked')).value;
+function divValue() {
     const name = document.getElementById("name").value;
-    const card = parent(document.getElementById("card")).value;
+    const card = document.getElementById("card").value;
     const address = document.getElementById("add").value;
 
     document.querySelector("span#person1").innerText = name;
     document.querySelector("span#person2").innerText = card;
     document.querySelector("span#person3").innerText = address;
+    document.querySelector("span#person4").innerText = name;
+    document.querySelector("span#person5").innerText = card;
+    document.querySelector("span#person6").innerText = address;
+    document.querySelector("span#person7").innerText = name;
+    document.querySelector("span#person8").innerText = card;
+    document.querySelector("span#person9").innerText = address;
+}
 
-    document.querySelector("div#jewelry1").removeAttribute("class");
-    document.querySelector("div#jewelry2").removeAttribute("class");
-    document.querySelector("div#jewelry3").removeAttribute("class");
+function formValue() {
+    const bracelet = document.getElementById("bra").checked;
+    const necklace = document.getElementById("nec").checked;
+    const earrings = document.getElementById("ear").checked;
 
-    if (program === "brac" && program === "neck" && program === "earr") {
+
+    if (bracelet && necklace && earrings) {
         document.getElementById("jewelry1").removeAttribute("class");
         document.getElementById("jewelry2").removeAttribute("class");
         document.getElementById("jewelry3").removeAttribute("class");
-    } else if (program === "brac" && program === "neck") {
+    } else if (bracelet && necklace) {
         document.getElementById("jewelry1").removeAttribute("class");
         document.getElementById("jewelry2").removeAttribute("class");
-    } else if (program === "brac" && program === "earr") {
+    } else if (bracelet && earrings) {
         document.getElementById("jewelry1").removeAttribute("class");
         document.getElementById("jewelry3").removeAttribute("class");
-    } else if (program === "neck" && program === "earr") {
+    } else if (necklace && earrings) {
         document.getElementById("jewelry2").removeAttribute("class");
         document.getElementById("jewelry3").removeAttribute("class");
-    } else if (program === "brac") {
+    } else if (bracelet) {
         document.getElementById("jewelry1").removeAttribute("class");
-    } else if (program === "neck") {
+    } else if (necklace) {
         document.getElementById("jewelry2").removeAttribute("class");
-    } else if (program === "earr") {
+    } else if (earrings) {
         document.getElementById("jewelry3").removeAttribute("class");
     }
 
 }
+function handleSubmission() {
+    const form = document.querySelector("form");
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        hiddenDiv();
+        formValue();
+        divValue();
+
+    });
+}
+
+window.addEventListener("load", handleSubmission);
